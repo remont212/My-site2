2d53d0203731969d779d47cef7204ee8478059a2
remont212 <107880286+remont212@users.noreply.github.com>
2022-07-28 17:57:30 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function sond() {
+  var s = new Audio();
+  s.src = "sounds/sound1.mp3"
+}
+
+function click1() {
+  alert("Артикул товара: /4567321");
+  sond();
+}
+
+
+function coldar() {
+  document.body.style.BackgraundColor("black");
+}
